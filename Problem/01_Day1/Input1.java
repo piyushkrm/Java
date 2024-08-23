cacb5c1b8e224e95ae63477ef9ba47c4fb677f8c
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-08-23 10:35:44 +0000

    Read and print integer values

---
+++ Problem/01_Day1/Input1.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Input1 {
+    public static void main(String[] args) {
+        //  In this java program we read an integer value and print it without recursion 
+
+        System.out.print("Enter number : ");
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println("Your inter the number is : "+n);
+    }
+}
