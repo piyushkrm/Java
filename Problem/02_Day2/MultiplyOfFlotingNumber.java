d43d98d79927aedb5d2175c24a757908478681bc
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-08-24 04:32:19 +0000

    multiplication of two floating number is solved

---
+++ Problem/02_Day2/MultiplyOfFlotingNumber.java
@@ -0,0 +1,9 @@
+public class MultiplyOfFlotingNumber {
+    public static void main(String[] args) {
+        float a = 10.5f;
+        float b = 2.3f;
+
+       float multiply = a * b;
+       System.out.println("Multiplication of two floating number are : "+multiply);
+     }
+}
