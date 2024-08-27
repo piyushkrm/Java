0d402b4f710bb19c6fe472fbf79e7754ea3fe829
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-08-27 04:46:55 +0000

    lcm problem are solved now i did it with function and input function

--- Problem/03_Day3/lcm1.java
+++ Problem/03_Day3/lcm1.java
@@ -5,5 +5,13 @@ public class lcm1 {
 
         int ans = (a > b) ? a : b;
         System.out.println(ans);
+
+        while (true) {
+            if (ans % a == 0 && ans % b == 0) {
+                break;
+            }
+            ans++;
+        }
+        System.out.println("LCM of " + a + " and " + b + " : " + ans);
     }
 }
