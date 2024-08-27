c5e10cd9ce2aafe53ef532029ac811438fb84ea5
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-08-27 04:34:42 +0000

    we start new day with new questions

---
+++ Problem/03_Day3/lcm1.java
@@ -0,0 +1,9 @@
+public class lcm1 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 15;
+
+        int ans = (a > b) ? a : b;
+        System.out.println(ans);
+    }
+}
