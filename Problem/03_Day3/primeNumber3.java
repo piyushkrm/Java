68a4c3049fda3c3322b0ca90cc4b961f624223b1
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-08-27 06:12:56 +0000

    i solved the prime number question

--- Problem/03_Day3/primeNumber3.java
+++ Problem/03_Day3/primeNumber3.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class primeNumber3 {
+
+    public static boolean isPrime(int number) {
+        if (number <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i < Math.sqrt(number); ++i) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
         // Find prime number from 1 to N number
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number to print the prime number from 1 : ");
+        int n = sc.nextInt();
+
+        for (int i = 2; i <= n; i++) {
+            if (isPrime(i)) {
+                System.out.print(i + " ");
+            }
+        }
     }
 }
