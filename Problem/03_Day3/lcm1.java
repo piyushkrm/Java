1f95c32abd529ca654906c38d27a880471eb5d86
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-08-27 04:52:55 +0000

    i did this problem with function and scanner class

--- Problem/03_Day3/lcm1.java
+++ Problem/03_Day3/lcm1.java
@@ -1,17 +1,23 @@
+import java.util.Scanner;
+
 public class lcm1 {
-    public static void main(String[] args) {
-        int a = 10;
-        int b = 15;
 
+    public static int findLCM(int a, int b) {
         int ans = (a > b) ? a : b;
-        System.out.println(ans);
-
         while (true) {
-            if (ans % a == 0 && ans % b == 0) {
+            if (ans % a == 0 && ans % b == 0) 
                 break;
-            }
             ans++;
         }
-        System.out.println("LCM of " + a + " and " + b + " : " + ans);
+        return ans;
+    }
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number 1 : ");
+        int a = sc.nextInt();
+        System.out.print("Enter number 2 : ");
+        int b = sc.nextInt();
+        System.out.println("LCM of " + a + " and " + b + " : " + findLCM(a, b));
     }
 }
