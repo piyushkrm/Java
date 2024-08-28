dc69a62cc715a2225d6ada773b1bce8fb02fee67
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-08-28 05:15:22 +0000

    approach 3 ie one line solution(ternary operator) using i solved factorial problem

--- Problem/04_Day4/calculateFact2.java
+++ Problem/04_Day4/calculateFact2.java
@@ -7,10 +7,10 @@ public class calculateFact2 {
         int result = 1;
         if (number == 0 || number == 1) {
             return 1;
-            
+
         }
         for (int i = 2; i <= number; i++) {
-            result*= i;
+            result *= i;
         }
         return result;
     }
@@ -20,15 +20,22 @@ public class calculateFact2 {
         if (number == 1 || number == 0) {
             return 1;
         }
-        return number * fact2(number-1);
+        return number * fact2(number - 1);
     }
+
+    // Approach 3
+    public static int fact3(int number) {
+        return (number == 0 || number == 1) ? 1 : number * fact3(number - 1);
+    }
+
     public static void main(String[] args) {
-        // calculate factorial 
+        // calculate factorial
         // Approach1
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter the number for calculating factorial : ");
         int number = sc.nextInt();
         System.out.println(fact1(number));
         System.out.println(fact2(number));
+        System.out.println(fact3(number));
     }
 }
