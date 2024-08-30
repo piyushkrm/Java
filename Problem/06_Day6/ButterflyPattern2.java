9e16424c83d4ef8552349aa7a70ece16c228b81a
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-08-30 11:20:05 +0530

    top left half is complete

--- Problem/06_Day6/ButterflyPattern2.java
+++ Problem/06_Day6/ButterflyPattern2.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class ButterflyPattern2 {
+
+    public static void print(int x) {
+        for (int i = 0; i <= x; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
     public static void main(String[] args) {
         // Butterfly Pattern: This pattern looks like a butterfly, with two symmetrical sides filled with stars.
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("Enter the height of the butterfly pattern : ");
+        // int n = sc.nextInt();
+        int n = 10;
+        print(n);
     }
 }
