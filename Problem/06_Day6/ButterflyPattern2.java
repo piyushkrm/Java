8be26e2bd1d2d761aebbf74bcf2cd07221ddc751
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-08-30 11:15:59 +0530

    New pattern question ie Butterfly Pattern

---
+++ Problem/06_Day6/ButterflyPattern2.java
@@ -0,0 +1,5 @@
+public class ButterflyPattern2 {
+    public static void main(String[] args) {
+        // Butterfly Pattern: This pattern looks like a butterfly, with two symmetrical sides filled with stars.
+    }
+}
