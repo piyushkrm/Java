c8512f88182c2455ddd01e6f37af1272ce1f5c7a
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-08-30 11:37:37 +0530

    Butterfly Pattern done

--- Problem/06_Day6/ButterflyPattern2.java
+++ Problem/06_Day6/ButterflyPattern2.java
@@ -2,20 +2,49 @@ import java.util.Scanner;
 
 public class ButterflyPattern2 {
 
-    public static void print(int x) {
-        for (int i = 0; i <= x; i++) {
-            for (int j = 0; j <= i; j++) {
+    public static void print(int n) {
+
+        // Upper half of the diamond
+        for (int i = 1; i <= n; i++) {
+            // Print left half of the row
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // Print spaces in the middle
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            // Print right half of the row
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // Lower half of the diamond
+        for (int i = n; i >= 1; i--) {
+            // Print left half of the row
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            // Print spaces in the middle
+            for (int j = 1; j <= 2 * (n - i); j++) {
+                System.out.print(" ");
+            }
+            // Print right half of the row
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
             System.out.println();
         }
     }
+
     public static void main(String[] args) {
-        // Butterfly Pattern: This pattern looks like a butterfly, with two symmetrical sides filled with stars.
-        // Scanner sc = new Scanner(System.in);
-        // System.out.print("Enter the height of the butterfly pattern : ");
-        // int n = sc.nextInt();
-        int n = 10;
+        // Butterfly Pattern: This pattern looks like a butterfly, with two symmetrical
+        // sides filled with stars.
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the height of the butterfly pattern : ");
+        int n = sc.nextInt();
         print(n);
     }
 }
