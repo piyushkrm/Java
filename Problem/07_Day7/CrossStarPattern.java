862963fb9e45bc770f120f8885ba0331238104cd
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-08-31 18:21:37 +0530

    Bottom part are also done

--- Problem/07_Day7/CrossStarPattern.java
+++ Problem/07_Day7/CrossStarPattern.java
@@ -19,19 +19,32 @@ public class CrossStarPattern {
             }
             System.out.println();
         }
+        for (int i = 0; i <= size; i++) {
+            for (int j = 0; j <= size; j++) {
+                if (i == size) {
+                    System.out.print("  ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.print("* ");
+
+            System.out.println();
+        }
     }
+
     public static void main(String[] args) {
         // Cross Star Pattern: This pattern prints a cross shape with stars.
         // int n = 5;
         // for (int i = 0; i < n; i++) {
-        //     for (int j = 0; j < n; j++) {
-        //         if (i == j || i + j == n - 1) {
-        //             System.out.print("* ");
-        //         } else {
-        //             System.out.print("  ");
-        //         }
-        //     }
-        //     System.out.println();
+        // for (int j = 0; j < n; j++) {
+        // if (i == j || i + j == n - 1) {
+        // System.out.print("* ");
+        // } else {
+        // System.out.print(" ");
+        // }
+        // }
+        // System.out.println();
         // }
         int n = 5;
         printPlusStar(n);
