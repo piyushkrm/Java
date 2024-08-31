d3e6825a30c76c1661bcd511d270a86e48ab6731
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-08-31 19:11:46 +0530

    Cross pattern are done

--- Problem/07_Day7/CrossStarPattern4.java
+++ Problem/07_Day7/CrossStarPattern4.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class CrossStarPattern4 {
 
     public static void crossPattern(int size) {
@@ -16,47 +18,32 @@ public class CrossStarPattern4 {
                     System.out.print("  ");
                 }
             }
-
             System.out.println();
         }
 
-
-        // for (int i = 0; i < size; i++) {
-        //     for (int j = 0; j < size; j++) {
-        //         if (i == 0 || i == size - 1 || j == 0 || j == size - 1) {
-        //             System.out.print("* ");
-        //         } else {
-        //             System.out.print("  ");
-        //         }
-        //     }
-        //     System.out.println();
-        // }
-        // System.out.println();
-        // for (int i = 0; i < size; i++) {
-        //     for (int j = 0; j < size; j++) {
-        //         if (i == 0 || i == size - 1 || j == 0 || j == size - 1) {
-        //             System.out.print("* ");
-        //         } else {
-        //             System.out.print("  ");
-        //         }
-        //     }
-        //     System.out.println();
-        // }
-        // System.out.println
+        for (int i = 0; i <= size; i++) {
+            for (int j = size; j >= 0; j--) {
+                if (i == j) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            for (int j = 0; j <= size; j++) {
+                if (i == j) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
     }
     public static void main(String[] args) {
         // Cross Star Pattern: This pattern prints a cross shape with stars.
-        int n = 5;
-        // for (int i = 0; i < n; i++) {
-        //     for (int j = 0; j < n; j++) {
-        //         if (i == j || i == n - j - 1) {
-        //             System.out.print("* ");
-        //         } else {
-        //             System.out.print("  ");
-        //         }
-        //     }
-        //     System.out.println();
-        // }
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value to print the pattern : ");
+        int n = sc.nextInt();
 
         crossPattern(n);
     }
