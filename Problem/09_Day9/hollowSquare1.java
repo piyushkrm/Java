a56f40e99814e37e42c4f69f9a50d2b922b995f5
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-06 16:33:50 +0530

    Hollow square pattern done

--- Problem/09_Day9/hollowSquare1.java
+++ Problem/09_Day9/hollowSquare1.java
@@ -4,7 +4,9 @@ public class hollowSquare1 {
     public static void print(int x) {
         for (int i = 0; i < x; i++) {
             for (int j = 0; j < x; j++) {
-                System.out.print("* ");
+                if (j == 0 || j == x) {
+                    System.out.print("* ");
+                }
             }
             System.out.println();
         }
