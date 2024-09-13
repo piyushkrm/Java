8904b380c1b8b969a9ffcdeb3c867f3e1c0372fa
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-13 10:53:44 +0530

    find Largest elemet in the array

--- Problem/10_Day10/arraySum.java
+++ Problem/10_Day10/arraySum.java
@@ -9,10 +9,27 @@ public class arraySum {
         }
         return sum;
     }
+
+    // find largest element
+    public static int findLargestElement(int[] array) {
+        int largestElement = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > largestElement) {
+                largestElement = array[i];
+            }
+        }
+
+        return largestElement;
+    }
+
+
     public static void main(String[] args) {
         // print sum of an array
-        int array[] = {10, 56, 100, 78, 59, 12};
+        int array[] = {10, 56, 120, 78, 59, 12};
         System.out.println("Sum of an array element's are : "+printSum(array));
+
+        // find Largest elemet in the array
+        System.out.println("Largest element in the array is : "+findLargestElement(array));
     }
     
 }
