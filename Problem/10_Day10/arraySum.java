7cef1578822696f7fd27392a08f8eabc1b3f345e
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-13 11:55:48 +0530

    Rmove duplicates

--- Problem/10_Day10/arraySum.java
+++ Problem/10_Day10/arraySum.java
@@ -23,13 +23,35 @@ public class arraySum {
     }
 
 
+    // remove duplicates element
+    public static void removeDuplicates(int array[]) {
+        boolean duplicates = false;
+
+        for (int i = 0; i < array.length; i++) {
+            duplicates = false;
+            for (int j = 0; j < i; j++) {
+                if (array[i] == array[j]) {
+                    duplicates = true;
+                    break;
+                }
+            }
+            if (duplicates) {
+                System.out.print(array[i] + " ");
+            }
+        }
+    }
+
+
     public static void main(String[] args) {
         // print sum of an array
-        int array[] = {10, 56, 120, 78, 59, 12};
-        System.out.println("Sum of an array element's are : "+printSum(array));
+        int array[] = {10, 56, 56, 45, 2, 15, 499, 53, 49, 120, 78, 78, 59, 12, 2, 12};
+        // System.out.println("Sum of an array element's are : "+printSum(array));
 
-        // find Largest elemet in the array
-        System.out.println("Largest element in the array is : "+findLargestElement(array));
+        // // find Largest elemet in the array
+        // System.out.println("Largest element in the array is : "+findLargestElement(array));
+
+        // remove duplicates from the array
+        removeDuplicates(array);
     }
     
 }
