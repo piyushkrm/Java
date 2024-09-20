30814107bedb05a57c9cad3ab99212f3deb66622
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-20 10:05:04 +0530

    This is the optimized way to find the major element

--- Problem/14_Day14/majorElements.java
+++ Problem/14_Day14/majorElements.java
@@ -1,5 +1,8 @@
+import java.util.Arrays;
+
 public class majorElements {
 
+    // brute force approach
     public static int findMajorElements(int[] nums) {
         int n = nums.length;
 
@@ -14,10 +17,36 @@ public class majorElements {
                 }
             }
         }
-        return 0;
+        return -1;
     }
+
+    // optimized way to find the major element
+    public static int findMajorityElement(int[] nums) {
+        int n = nums.length;
+        int freq = 1;
+        int ans = nums[0];
+
+        // sort the array
+        Arrays.sort(nums);
+
+        for (int i = 1; i < n; i++) {
+            if (nums[i] == nums[i-1]) {
+                freq++;
+            } else {
+                freq = 1;
+                ans = nums[i];
+            }
+            if (freq > n / 2) {
+                return ans;
+            }
+        }
+
+        return -1;
+    }
+
     public static void main(String[] args) {
-        int[] array = {1, 2, 2, 1, 1};
-        System.out.println("The majority element is: " + findMajorElements(array));
+        int[] array = {1, 2, 2, 1, 1, 2, 2, 2};
+        // System.out.println("The majority element is: " + findMajorElements(array));
+        System.out.println("The majority element is: " + findMajorityElement(array));
     }
 }
