fd70e76c92fa2b9af7cc46b6b2ed3128428cad9e
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-09-27 10:22:10 +0530

    Test the optimized solution

--- Problem/16_Day16/leetcode11.java
+++ Problem/16_Day16/leetcode11.java
@@ -3,6 +3,8 @@
 
 public class leetcode11 {
 
+    // Brute force approach
+
     public static int maxWater(int container[]) {
         int n = container.length;
         int maxAreaWater = 0;
@@ -18,10 +20,35 @@ public class leetcode11 {
         return maxAreaWater;
     }
 
+    // Optimal solution with two pointer approach
+    public static int maxWaterOptimized(int container[]) {
+        int n = container.length;
+        int maxAreaWater = 0;
+        int leftPointer = 0;
+        int rightPointer = n - 1;
+
+        while(leftPointer < rightPointer) {
+            int containerWidth = rightPointer - leftPointer;
+            int containerHeight = Math.min(container[leftPointer], container[rightPointer]);
+            int area = containerWidth * containerHeight;
+            maxAreaWater = Math.max(maxAreaWater, area);
+            // Move the pointer associated with the smaller height
+            if (container[leftPointer] < container[rightPointer]) {
+                leftPointer++;
+            } else {
+                rightPointer--;
+            }
+        }
+        return maxAreaWater;
+    }
+
     public static void main(String[] args) {
         int[] heights = {1, 8, 6, 2, 5, 4, 8, 3, 7};
         int result = maxWater(heights);
         System.out.println("Maximum water that can be stored: " + result);
         
+        // Test the optimized solution
+        int ans = maxWaterOptimized(heights);
+        System.out.println("Maximum water that can be stored (optimized): " + ans);
     }
 }
