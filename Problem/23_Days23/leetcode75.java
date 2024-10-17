1e979ea8361ea23dd9365eb9840c8ce9955f9cf9
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-10-17 10:58:08 +0530

    Sort optimal approach

--- Problem/23_Days23/leetcode75.java
+++ Problem/23_Days23/leetcode75.java
@@ -11,11 +11,44 @@ public class leetcode75 {
         System.out.println();
     }
 
+    // Approach 2 
     public static void sortColorsBruteForce(int[] colors) {
         Arrays.sort(colors);
         printArray(colors);
     }
 
+    // Approach 3: Optimal approach
+    public static void sortColorsOptimized(int[] arr) {
+        int count0 = 0;
+        int count1 = 0;
+        int count2 = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 0) {
+                count0++;
+            } else if (arr[i] == 1) {
+                count1++;
+            } else {
+                count2++;
+            }
+        }
+
+        int index = 0;
+        for (int i = 0; i < count0; i++) {
+            arr[index++] = 0;
+        }
+
+        for (int i = 0; i < count1; i++) {
+            arr[index++] = 1;
+        }
+
+        for (int i = 0; i < count2; i++) {
+            arr[index++] = 2;
+        }
+        printArray(arr);
+    }
+
+    // Approach 1: Dutch National Flag Problem
     public static void sortColors(int[] arr) {
         int low = 0; 
         int mid = 0;
@@ -44,6 +77,7 @@ public class leetcode75 {
         int[] arr = { 2, 0, 2, 1, 1, 0 };
         sortColors(arr);
         sortColorsBruteForce(arr);
+        sortColorsOptimized(arr);
 
     }
 }
