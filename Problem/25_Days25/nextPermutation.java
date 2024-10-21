193f990b7370b0853ddbb1470769d62e1b1bdb75
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-10-21 17:34:07 +0530

    Next permutation function are on the process

--- Problem/25_Days25/nextPermutation.java
+++ Problem/25_Days25/nextPermutation.java
@@ -1,5 +1,31 @@
 public class nextPermutation {
     // leet code 31 problem
 
-    
+    public static void nextPermutation(int[] nums) {
+        int pivot = -1;
+        int n = nums.length;
+
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
+                pivot = i;
+                break;
+            }
+        }
+
+        if (pivot == -1) {
+            reverse(nums, 0, n - 1);
+            return;
+        }
+
+        for (int i = n - 1; i > pivot; i--) {
+            if (nums[i] > nums[pivot]) {
+                int temp = nums[i];
+                nums[i] = nums[pivot];
+                nums[pivot] = temp;
+                break;
+            }
+        }
+
+        reverse(nums, pivot + 1, n - 1);
+    }
 }
