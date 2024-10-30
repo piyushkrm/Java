6724a5ade1cfa0bd14f9e889cc7c601dc8a2c10c
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-10-30 11:50:57 +0530

    Create list

---
+++ Problem/26_Days26/listContainer.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+//List container
+
+
+
+
+
+public class listContainer {
+    public static void main(String[] args) {
+        // Create list
+        List<Integer>list = new ArrayList<>();
+
+
+    }
+}
