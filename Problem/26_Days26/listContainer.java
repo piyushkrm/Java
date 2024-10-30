98db640cb3de974c26abfc6c8bd5a360ea46d8a7
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-10-30 12:00:17 +0530

    Done common operation on the list

--- Problem/26_Days26/listContainer.java
+++ Problem/26_Days26/listContainer.java
@@ -3,35 +3,37 @@ import java.util.*;
 //List container
 public class listContainer {
 
-// Print the list element
-public static void printList(List<Integer> list) {
-    for (Integer i : list) { 
-        System.out.print(i + " ");
+    // Print the list element
+    public static void printList(List<Integer> list) {
+        for (Integer i : list) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
     }
-    System.out.println();
-}
-
-
 
     public static void main(String[] args) {
         // Create list
-        List<Integer>list = new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
 
         // Add element
+        list.add(15);
         list.add(10);
         list.add(13);
-        list.add(15);
         list.add(17);
-        printList(list);        // print list of elements
-        
-        // Get element  
-        System.out.println("Element at the index 2 is : "+ list.get(2)); // get element from list at position 2
+        printList(list); // print list of elements
+
+        // Get element
+        System.out.println("Element at the index 2 is : " + list.get(2)); // get element from list at position 2
 
         // Add element on the at specified index
         list.add(1, 12);
         printList(list);
-        
 
-        
+        // Remove
+        list.remove(1);
+        printList(list);
+
+        // Size
+        System.out.println("Size of the list is : " + list.size());
     }
 }
