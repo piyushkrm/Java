fe3504ee85fb4d6527900d10b70389c94025f2c2
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-10-30 11:55:59 +0530

    Get the element at the specific index

--- Problem/26_Days26/listContainer.java
+++ Problem/26_Days26/listContainer.java
@@ -1,6 +1,7 @@
 import java.util.*;
 
 //List container
+public class listContainer {
 
 // Print the list element
 public static void printList(List<Integer> list) {
@@ -12,13 +13,20 @@ public static void printList(List<Integer> list) {
 
 
 
-public class listContainer {
     public static void main(String[] args) {
         // Create list
         List<Integer>list = new ArrayList<>();
 
         // Add element
         list.add(10);
+        list.add(13);
+        list.add(15);
+        list.add(17);
+        printList(list);        // print list of elements
+        
+        // Get element  
+        System.out.println("Element at the index 2 is : "+ list.get(2)); // get element from list at position 2
+
         
     }
 }
