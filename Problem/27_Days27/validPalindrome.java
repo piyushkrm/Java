7922986ab5ee745aeff07654d6cc3263bba931a7
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-10-31 10:13:20 +0530

    Leetcode problem 125 is done sucessfully

---
+++ Problem/27_Days27/validPalindrome.java
@@ -0,0 +1,37 @@
+
+// Leet code 125 --> Is validPalindrome
+
+public class validPalindrome {
+
+    public static boolean isAlphaNumeric(int c) {
+        return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z') || (c >= '0' && c <= '9');
+    }
+
+    public static boolean isPalindrome(String s) {
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (!isAlphaNumeric(left)) {
+                left++;
+                continue;
+            }
+            if (!isAlphaNumeric(right)) {
+                right--;
+                continue;
+            }
+
+            if (Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        // IsValid Palindrome
+        String str = "A man, a plan, a canal: Panama";
+        System.out.println(isPalindrome(str));
+    }
+}
