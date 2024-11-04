b3ef9f193026efb690a704e5cb640c59bd802d20
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-11-04 17:07:59 +0530

    checkInclusion function done

--- Problem/29_Days29/permutationInString.java
+++ Problem/29_Days29/permutationInString.java
@@ -1,40 +1,44 @@
 
 // 3163. String Compression III
 
-
 public class permutationInString {
 
-    public static boolean checkInclusion(String s1, String s2) {
-        int freq[] = {0};
 
-        for(int i = 0; i < s1.length(); i++) {
+    public static boolean checkInclusion(String s1, String s2) {
+        if (s1.length() > s2.length())
+            return false;
+
+        int[] freq = new int[26];
+        int[] windowFrequency = new int[26];
+
+        // Fill the frequency array for s1
+        for (int i = 0; i < s1.length(); i++) {
             freq[s1.charAt(i) - 'a']++;
         }
 
-        int windowSize = s1.length();
-        for(int i = 0; i < s2.length(); i++) {
-            int windowIdx = 0;
-            int idx = i;
-            int windowFrequency[] = {0};
-
-            while(windowIdx < windowSize && idx < s2.length()) {
-                windowFrequency[s2.charAt(idx) - 'a']++;
-                windowIdx++;
-                idx++;
-
-                if(isFrequency(freq, windowFrequency)) {
-                    return true;
-                }
-                return false;
-            }
+        // Initialize the window frequency array for the first window
+        for (int i = 0; i < s1.length(); i++) {
+            windowFrequency[s2.charAt(i) - 'a']++;
         }
 
+        // Slide the window over s2
+        for (int i = 0; i <= s2.length() - s1.length(); i++) {
+            if (i > 0) {
+                // Update the window by removing the character going out of the window
+                // and adding the new character coming into the window
+                windowFrequency[s2.charAt(i - 1) - 'a']--;
+                windowFrequency[s2.charAt(i + s1.length() - 1) - 'a']++;
+            }
+
+            // Check if the current window matches the frequency of s1
+            if (isFrequency(freq, windowFrequency)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     public static void main(String[] args) {
-        String s1 = "ab";
-        String s2 = "eidbaooo";
 
-        }
-    
+    }
 }
