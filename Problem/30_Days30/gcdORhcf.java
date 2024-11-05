ad175a6ecc8738564913133fdcc789a0fc9182a3
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-11-05 18:10:48 +0530

    Optimal approach to find the HCF and GCD values using Euclidean Algorithm

--- Problem/30_Days30/gcdORhcf.java
+++ Problem/30_Days30/gcdORhcf.java
@@ -5,9 +5,12 @@ public class gcdORhcf {
 
     // Function to find GCD
     public static int gcd(int x, int y) {
-        if (x == 0) return y;
-        if (y == 0) return x;
-        if (x == y) return x;
+        if (x == 0)
+            return y;
+        if (y == 0)
+            return x;
+        if (x == y)
+            return x;
 
         int gcd = 1;
         for (int i = 1; i < Math.min(x, y); i++) {
@@ -18,10 +21,25 @@ public class gcdORhcf {
         return gcd;
     }
 
+    // Optimal approach to find the HCF and GCD values using Euclidean Algorithm
+    public static int gcdOptimalApproach(int x, int y) {
+        while (x > 0 && y > 0) {
+            if (x > y) {
+                // x -= y;
+                x %= y;
+            } else {
+                // y -= x;
+                y %= x;
+            }
+        }
+        return x + y;
+    }
+
     public static void main(String[] args) {
         int x = 20;
         int y = 28;
         System.out.println("GCD of " + x + " and " + y + " is: " + gcd(x, y));
+        System.out.println("HCF of " + x + " and " + y + " is: " + gcdOptimalApproach(x, y));
     }
 
 }
