23930a54f07373e3c7bb0cf95528bb6aaf318ae2
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-11-05 16:45:00 +0530

    Check numbers are prime numbers in a range approach 2

--- Problem/30_Days30/primeNumber.java
+++ Problem/30_Days30/primeNumber.java
@@ -15,20 +15,45 @@ public class primeNumber {
         return "Prime number";
     }
 
-    // Check numbers are prime numbers in a range
+    // Check numbers are prime numbers in a range approach 1
     public static void checkPrimeInRange(int start, int end) {
         for (int i = start; i <= end; i++) {
             System.out.println(i + " : " + isPrime(i));
         }
     }
 
+    // Check numbers are prime numbers in a range approach 2
+    public static void checkPrimeInRange2(int start, int end) {
+        for (int num = start; num <= end; num++) {
+            if (num < 0)
+                continue;
+            boolean isPrime = true;
+            for (int i = 2; i * i <= num; i++) {
+                if (num % i == 0) {
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime) {
+                System.out.println(num + " : Prime number");
+            } else {
+                System.out.println(num + " : Not a prime number");
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int number = 24;
         System.out.println(isPrime(number)); // Output: Not a prime number
-        
+        System.out.println();
+
         int start = 1;
         int end = 20;
         checkPrimeInRange(start, end); // Output: 1 : Not a prime number
+        System.out.println();
 
+        start = 100;
+        end = 200;
+        checkPrimeInRange2(start, end); // Output: 1 : Not a prime number
     }
 }
