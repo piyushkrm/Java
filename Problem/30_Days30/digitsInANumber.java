26af2d8d037f5cfe5ba8a0503a4daa4e6d7a0d40
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-11-05 17:33:56 +0530

    Count of digits in a number

---
+++ Problem/30_Days30/digitsInANumber.java
@@ -0,0 +1,19 @@
+
+// Digits in a number
+
+public class digitsInANumber {
+
+    // Count of digits in a number
+    public static int countOfDigits(int n) {
+        int count = 0;
+        while (n!=0) {
+            n = n/10;
+            count++;
+        }
+        return count;
+    }
+    public static void main(String[] args) {
+        int number = 123456;
+        System.out.println("Number of digits in " + number + " is: " + countOfDigits(number));
+    }
+}
