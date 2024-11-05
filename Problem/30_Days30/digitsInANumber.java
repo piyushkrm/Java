a06bc050cb6d5f37bfbe7e69a0cde5fd85fc2b8a
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-11-05 17:39:57 +0530

    Print the digits of the number

--- Problem/30_Days30/digitsInANumber.java
+++ Problem/30_Days30/digitsInANumber.java
@@ -6,14 +6,26 @@ public class digitsInANumber {
     // Count of digits in a number
     public static int countOfDigits(int n) {
         int count = 0;
-        while (n!=0) {
-            n = n/10;
+        while (n != 0) {
+            n = n / 10;
             count++;
         }
         return count;
     }
+
+    // Print the digits of the number
+    public static void printDigits(int n) {
+        System.out.print("Digits of " + n + " are: ");
+        while (n != 0) {
+            int digit = n % 10;
+            System.out.print(digit + " ");
+            n = n / 10;
+        }
+    }
+
     public static void main(String[] args) {
         int number = 123456;
         System.out.println("Number of digits in " + number + " is: " + countOfDigits(number));
+        printDigits(number);
     }
 }
